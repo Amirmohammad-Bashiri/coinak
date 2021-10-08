9f46006d4bf2c62a51a1d9f811821bdc55c336e7
Amirmohammad-Bashiri <amirmohamad.bashiri@gmail.com>
2021-10-08 16:49:01 +0330

    Coin card setup

---
+++ components/home-page/trending-coin-card/trending-coin-card.js
@@ -0,0 +1,26 @@
+import Image from "next/image";
+
+import styles from "./trending-coin-card.module.scss";
+
+import coinImage from "./dogecoin.png";
+
+function TrendingCoinCard() {
+  return (
+    <div className={`${styles.card} bg-gray-8 text-gray-2 p-2`}>
+      <div className={styles.card__header}>
+        <h3>Bitcoin</h3>
+        <Image src={coinImage} alt="bitcoin" width={30} height={30} />
+      </div>
+
+      <div className={styles["card__header--divide"]}></div>
+
+      <div className={`${styles.card__body} text-gray-3`}>
+        <p>Price: 44.5K</p>
+        <p>Market Cap: 836.78</p>
+        <p>Daily Change: -3.37%</p>
+      </div>
+    </div>
+  );
+}
+
+export default TrendingCoinCard;
--- components/home-page/trending-coins-list/trending-coins-list.js
+++ components/home-page/trending-coins-list/trending-coins-list.js
@@ -1,5 +1,13 @@
+import TrendingCoinCard from "../trending-coin-card/trending-coin-card";
+
+import styles from "./trending-coin-list.module.scss";
+
 function TrendingCoinsList() {
-  return <div>Trending Coins List</div>;
+  return (
+    <div>
+      <TrendingCoinCard />
+    </div>
+  );
 }
 
 export default TrendingCoinsList;
