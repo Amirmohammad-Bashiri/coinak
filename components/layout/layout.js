5c5d68cd5ae2148c4eff72847567b6a192a823c3
Amirmohammad-Bashiri <amirmohamad.bashiri@gmail.com>
2021-10-08 11:28:02 +0330

    Implemented navbar

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,11 +1,14 @@
 import Head from "next/head";
 
+import Navbar from "../navbar/navbar";
+
 function layout({ children }) {
   return (
     <div>
       <Head>
         <title>Coinak | Get Latest Crypto Info</title>
       </Head>
+      <Navbar />
       {children}
     </div>
   );
---
+++ components/navbar/navbar.js
@@ -0,0 +1,33 @@
+import Link from "next/link";
+
+import styles from "./navbar.module.scss";
+
+function Navbar() {
+  return (
+    <header className="bg-gray-5 text-gray-1 p-2">
+      <nav className={styles.navbar}>
+        <div className={styles.logo}>
+          <Link href="/">
+            <a>Coinak</a>
+          </Link>
+        </div>
+
+        <div>
+          <ul className={styles.navbar__links}>
+            <li className="text-hover-emerald-2">
+              <a href="#trending">Trending</a>
+            </li>
+            <li className="text-hover-emerald-2">
+              <a href="#currencies">Currencies</a>
+            </li>
+            <li className="text-hover-emerald-2">
+              <a href="#news">News</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Navbar;
