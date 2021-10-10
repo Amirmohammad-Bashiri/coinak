ef29c669ebcdd0b2f8005e7fe2e2f663d16e9b0d
Amirmohammad-Bashiri <amirmohamad.bashiri@gmail.com>
2021-10-10 12:54:39 +0330

    Currency table setup

--- components/home-page/currencies-section/currencies-section.js
+++ components/home-page/currencies-section/currencies-section.js
@@ -1,5 +1,21 @@
+import CurrenciesTable from "./currencies-table/currencies-table";
+
+import styles from "./currencies-section.module.scss";
+
 function CurrenciesSection() {
-  return <section>currencies section</section>;
+  return (
+    <section className="bg-gray-8">
+      <div className={styles.currencies}>
+        <div className={styles.currencies__header}>
+          <h1>Cryptocurrency Prices by Market Cap</h1>
+        </div>
+
+        <div className={styles.currencies__table__container}>
+          <CurrenciesTable />
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default CurrenciesSection;
--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ["assets.coingecko.com"],
+    domains: ["assets.coingecko.com", "www.coingecko.com"],
   },
 };
