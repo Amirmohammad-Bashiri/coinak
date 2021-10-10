c8ba23a833525ad439331ed5895e87e0efde3774
Amirmohammad-Bashiri <amirmohamad.bashiri@gmail.com>
2021-10-10 17:56:11 +0330

    Added tbody

--- components/home-page/currencies-section/currencies-table/currencies-table.js
+++ components/home-page/currencies-section/currencies-table/currencies-table.js
@@ -17,147 +17,148 @@ function CurrenciesTable() {
             <th>Last 7 Days</th>
           </tr>
         </thead>
-
-        <tr>
-          <td>
-            <div className={styles.currencies__table__coin}>
+        <tbody>
+          <tr>
+            <td>
+              <div className={styles.currencies__table__coin}>
+                <Image
+                  src="https://assets.coingecko.com/coins/images/1/thumb/bitcoin.png?1547033579"
+                  alt="bitcoin"
+                  width={20}
+                  height={20}
+                />{" "}
+                <span>Bitcoin</span>
+              </div>
+            </td>
+            <td>BTC</td>
+            <td>$55,651.80</td>
+            <td>-3.4%</td>
+            <td>$36,086,993,736</td>
+            <td>$6,806,011</td>
+            <td className={styles.currencies__table__sparkline}>
               <Image
-                src="https://assets.coingecko.com/coins/images/1/thumb/bitcoin.png?1547033579"
-                alt="bitcoin"
-                width={20}
-                height={20}
-              />{" "}
-              <span>Bitcoin</span>
-            </div>
-          </td>
-          <td>BTC</td>
-          <td>$55,651.80</td>
-          <td>-3.4%</td>
-          <td>$36,086,993,736</td>
-          <td>$6,806,011</td>
-          <td className={styles.currencies__table__sparkline}>
-            <Image
-              src="https://www.coingecko.com/coins/975/sparkline"
-              alt="sparkline"
-              layout="responsive"
-              height={30}
-              width="100%"
-              objectFit="contain"
-            />
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <div className={styles.currencies__table__coin}>
+                src="https://www.coingecko.com/coins/975/sparkline"
+                alt="sparkline"
+                layout="responsive"
+                height={30}
+                width="100%"
+                objectFit="contain"
+              />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <div className={styles.currencies__table__coin}>
+                <Image
+                  src="https://assets.coingecko.com/coins/images/1/thumb/bitcoin.png?1547033579"
+                  alt="bitcoin"
+                  width={20}
+                  height={20}
+                />{" "}
+                <span>Bitcoin</span>
+              </div>
+            </td>
+            <td>BTC</td>
+            <td>$55,651.80</td>
+            <td>1.2%</td>
+            <td>$36,086,993,736</td>
+            <td>$1,048,334,984,400</td>
+            <td className={styles.currencies__table__sparkline}>
               <Image
-                src="https://assets.coingecko.com/coins/images/1/thumb/bitcoin.png?1547033579"
-                alt="bitcoin"
-                width={20}
-                height={20}
-              />{" "}
-              <span>Bitcoin</span>
-            </div>
-          </td>
-          <td>BTC</td>
-          <td>$55,651.80</td>
-          <td>1.2%</td>
-          <td>$36,086,993,736</td>
-          <td>$1,048,334,984,400</td>
-          <td className={styles.currencies__table__sparkline}>
-            <Image
-              src="https://www.coingecko.com/coins/1/sparkline"
-              alt="sparkline"
-              layout="responsive"
-              height={30}
-              width="100%"
-              objectFit="contain"
-            />
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <div className={styles.currencies__table__coin}>
+                src="https://www.coingecko.com/coins/1/sparkline"
+                alt="sparkline"
+                layout="responsive"
+                height={30}
+                width="100%"
+                objectFit="contain"
+              />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <div className={styles.currencies__table__coin}>
+                <Image
+                  src="https://assets.coingecko.com/coins/images/1/thumb/bitcoin.png?1547033579"
+                  alt="bitcoin"
+                  width={20}
+                  height={20}
+                />{" "}
+                <span>Bitcoin</span>
+              </div>
+            </td>
+            <td>BTC</td>
+            <td>$55,651.80</td>
+            <td>1.2%</td>
+            <td>$36,086,993,736</td>
+            <td>$1,048,334,984,400</td>
+            <td className={styles.currencies__table__sparkline}>
               <Image
-                src="https://assets.coingecko.com/coins/images/1/thumb/bitcoin.png?1547033579"
-                alt="bitcoin"
-                width={20}
-                height={20}
-              />{" "}
-              <span>Bitcoin</span>
-            </div>
-          </td>
-          <td>BTC</td>
-          <td>$55,651.80</td>
-          <td>1.2%</td>
-          <td>$36,086,993,736</td>
-          <td>$1,048,334,984,400</td>
-          <td className={styles.currencies__table__sparkline}>
-            <Image
-              src="https://www.coingecko.com/coins/1/sparkline"
-              alt="sparkline"
-              layout="responsive"
-              height={30}
-              width="100%"
-              objectFit="contain"
-            />
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <div className={styles.currencies__table__coin}>
+                src="https://www.coingecko.com/coins/1/sparkline"
+                alt="sparkline"
+                layout="responsive"
+                height={30}
+                width="100%"
+                objectFit="contain"
+              />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <div className={styles.currencies__table__coin}>
+                <Image
+                  src="https://assets.coingecko.com/coins/images/1/thumb/bitcoin.png?1547033579"
+                  alt="bitcoin"
+                  width={20}
+                  height={20}
+                />{" "}
+                <span>Bitcoin</span>
+              </div>
+            </td>
+            <td>BTC</td>
+            <td>$55,651.80</td>
+            <td>1.2%</td>
+            <td>$36,086,993,736</td>
+            <td>$1,048,334,984,400</td>
+            <td className={styles.currencies__table__sparkline}>
               <Image
-                src="https://assets.coingecko.com/coins/images/1/thumb/bitcoin.png?1547033579"
-                alt="bitcoin"
-                width={20}
-                height={20}
-              />{" "}
-              <span>Bitcoin</span>
-            </div>
-          </td>
-          <td>BTC</td>
-          <td>$55,651.80</td>
-          <td>1.2%</td>
-          <td>$36,086,993,736</td>
-          <td>$1,048,334,984,400</td>
-          <td className={styles.currencies__table__sparkline}>
-            <Image
-              src="https://www.coingecko.com/coins/1/sparkline"
-              alt="sparkline"
-              layout="responsive"
-              height={30}
-              width="100%"
-              objectFit="contain"
-            />
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <div className={styles.currencies__table__coin}>
+                src="https://www.coingecko.com/coins/1/sparkline"
+                alt="sparkline"
+                layout="responsive"
+                height={30}
+                width="100%"
+                objectFit="contain"
+              />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <div className={styles.currencies__table__coin}>
+                <Image
+                  src="https://assets.coingecko.com/coins/images/1/thumb/bitcoin.png?1547033579"
+                  alt="bitcoin"
+                  width={20}
+                  height={20}
+                />{" "}
+                <span>Bitcoin</span>
+              </div>
+            </td>
+            <td>BTC</td>
+            <td>$55,651.80</td>
+            <td>1.2%</td>
+            <td>$36,086,993,736</td>
+            <td>$1,048,334,984,400</td>
+            <td className={styles.currencies__table__sparkline}>
               <Image
-                src="https://assets.coingecko.com/coins/images/1/thumb/bitcoin.png?1547033579"
-                alt="bitcoin"
-                width={20}
-                height={20}
-              />{" "}
-              <span>Bitcoin</span>
-            </div>
-          </td>
-          <td>BTC</td>
-          <td>$55,651.80</td>
-          <td>1.2%</td>
-          <td>$36,086,993,736</td>
-          <td>$1,048,334,984,400</td>
-          <td className={styles.currencies__table__sparkline}>
-            <Image
-              src="https://www.coingecko.com/coins/1/sparkline"
-              alt="sparkline"
-              layout="responsive"
-              height={30}
-              width="100%"
-              objectFit="contain"
-            />
-          </td>
-        </tr>
+                src="https://www.coingecko.com/coins/1/sparkline"
+                alt="sparkline"
+                layout="responsive"
+                height={30}
+                width="100%"
+                objectFit="contain"
+              />
+            </td>
+          </tr>
+        </tbody>
       </table>
     </div>
   );
