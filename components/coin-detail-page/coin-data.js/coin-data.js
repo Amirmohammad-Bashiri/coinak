a11c8782b179cb20ebba220c82e1cf1b0ccf068b
Amirmohammad-Bashiri <amirmohamad.bashiri@gmail.com>
2021-10-19 17:11:22 +0330

    Extracted price statistics into a component

--- components/coin-detail-page/coin-data.js/coin-data.js
+++ components/coin-detail-page/coin-data.js/coin-data.js
@@ -1,20 +1,15 @@
 import { useState } from "react";
-import Image from "next/image";
 
 import { useMarketData } from "@hooks/useMarketData";
-import { priceFormatter } from "@utils/priceFormatter";
 import CoinChart from "../coin-chart/coin-chart";
 
 import styles from "./coin-data.module.scss";
+import CoinPriceStatistics from "../coin-price-statistics/coin-price-statistics";
 
 function CoinData({ coinId, coin }) {
   const [currency, setCurrency] = useState("usd");
   const [days, setDays] = useState("1");
   const { marketData, loading } = useMarketData(currency, coinId, days);
-
-  const priceChangePercentage = coin.price_change_percentage_24h.toFixed(2);
-  const isPriceDown = String(priceChangePercentage).startsWith("-");
-  const priceChangeClass = isPriceDown ? "price__down" : "price__up";
 
   const handleCurrencyChange = e => {
     setCurrency(e.target.value);
@@ -44,56 +39,7 @@ function CoinData({ coinId, coin }) {
         <CoinChart marketData={marketData} />
       </div>
 
-      <div className={styles.price}>
-        <div className={styles.price__header}>
-          <div className={styles.price__header__logo}>
-            <Image src={coin.image} alt="Coin Logo" width={70} height={70} />
-          </div>
-          <div className={styles.price__header__info}>
-            <p className={styles.price__header__info__symbol}>
-              {coin.symbol.toUpperCase()}
-            </p>
-            <p>Price statistics</p>
-          </div>
-        </div>
-        <div className={styles.divider}></div>
-        <div className={styles.price__body}>
-          <div className={styles.price__body__item}>
-            <p>Price</p>
-            <span>{priceFormatter(coin.current_price)}</span>
-          </div>
-          <div className={styles.divider}></div>
-          <div className={styles.price__body__item}>
-            <p>Price Change</p>
-            <span className={styles[priceChangeClass]}>
-              {priceFormatter(coin.price_change_24h)}
-            </span>
-          </div>
-          <div className={styles.divider}></div>
-          <div className={styles.price__body__item}>
-            <p>Price Change Percentage</p>
-            <span className={styles[priceChangeClass]}>
-              {priceChangePercentage}%
-            </span>
-          </div>
-          <div className={styles.divider}></div>
-          <div className={styles.price__body__item}>
-            <p>24h High</p>
-            <span>{coin.high_24h}</span>
-          </div>
-          <div className={styles.divider}></div>
-          <div className={styles.price__body__item}>
-            <p>24h Low</p>
-            <span>{coin.low_24h}</span>
-          </div>
-          <div className={styles.divider}></div>
-          <div className={styles.price__body__item}>
-            <p>Volume</p>
-            <span>{priceFormatter(coin.total_volume)}</span>
-          </div>
-          <div className={styles.divider}></div>
-        </div>
-      </div>
+      <CoinPriceStatistics coin={coin} />
     </div>
   );
 }
---
+++ components/coin-detail-page/coin-price-statistics/coin-price-statistics.js
@@ -0,0 +1,66 @@
+import Image from "next/image";
+
+import { priceFormatter } from "@utils/priceFormatter";
+
+import styles from "./coin-price-statistics.module.scss";
+
+function CoinPriceStatistics({ coin }) {
+  const priceChangePercentage = coin.price_change_percentage_24h.toFixed(2);
+  const isPriceDown = String(priceChangePercentage).startsWith("-");
+  const priceChangeClass = isPriceDown ? "price__down" : "price__up";
+
+  return (
+    <div className={styles.price}>
+      <div className={styles.price__header}>
+        <div className={styles.price__header__logo}>
+          <Image src={coin.image} alt="Coin Logo" width={70} height={70} />
+        </div>
+        <div className={styles.price__header__info}>
+          <p className={styles.price__header__info__symbol}>
+            {coin.symbol.toUpperCase()}
+          </p>
+          <p>Price statistics</p>
+        </div>
+      </div>
+      <div className={styles.divider}></div>
+      <div className={styles.price__body}>
+        <div className={styles.price__body__item}>
+          <p>Price</p>
+          <span>{priceFormatter(coin.current_price)}</span>
+        </div>
+        <div className={styles.divider}></div>
+        <div className={styles.price__body__item}>
+          <p>Price Change</p>
+          <span className={styles[priceChangeClass]}>
+            {priceFormatter(coin.price_change_24h)}
+          </span>
+        </div>
+        <div className={styles.divider}></div>
+        <div className={styles.price__body__item}>
+          <p>Price Change Percentage</p>
+          <span className={styles[priceChangeClass]}>
+            {priceChangePercentage}%
+          </span>
+        </div>
+        <div className={styles.divider}></div>
+        <div className={styles.price__body__item}>
+          <p>24h High</p>
+          <span>{coin.high_24h}</span>
+        </div>
+        <div className={styles.divider}></div>
+        <div className={styles.price__body__item}>
+          <p>24h Low</p>
+          <span>{coin.low_24h}</span>
+        </div>
+        <div className={styles.divider}></div>
+        <div className={styles.price__body__item}>
+          <p>Volume</p>
+          <span>{priceFormatter(coin.total_volume)}</span>
+        </div>
+        <div className={styles.divider}></div>
+      </div>
+    </div>
+  );
+}
+
+export default CoinPriceStatistics;
